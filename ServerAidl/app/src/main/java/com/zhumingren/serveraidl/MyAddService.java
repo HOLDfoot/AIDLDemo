71f80499f8e076dfff0fd498acf24c231e3373bc
zhuYongle <iamzhulele@163.com>
2023-02-10 09:22:50 +0800

    可以成功作为服务端发送图片

--- ServerAidl/app/src/main/java/com/zhumingren/serveraidl/MyAddService.java
+++ ServerAidl/app/src/main/java/com/zhumingren/serveraidl/MyAddService.java
@@ -2,10 +2,15 @@ package com.zhumingren.serveraidl;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
 
 import com.zhumingren.serveraidl.aidlfolder.IMyAidlInterface;
+
+import java.io.ByteArrayOutputStream;
 
 public class MyAddService extends Service {
     public MyAddService() {
@@ -26,6 +31,17 @@ public class MyAddService extends Service {
         @Override
         public int add(int a, int b) throws RemoteException {
             return a + b;
+        }
+
+        @Override
+        public byte[] getOriginBitmapBytes() throws RemoteException {
+            //Bitmap bitmap = BitmapFactory.decodeFile("/sdcard/1/surfacebitmap.png");
+            Bitmap bitmap = BitmapFactory.decodeResource(MyAddService.this.getResources(), R.drawable.surface_bitmap);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            byte[] bytes = outputStream.toByteArray();
+            Log.e("zyl", "getOriginBitmapBytes bytes length: " + bytes.length);
+            return bytes;
         }
     }
 }
